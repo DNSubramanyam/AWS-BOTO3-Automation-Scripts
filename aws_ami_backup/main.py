84faa2aee33c60fcf5fb8475d52e57a9fb08d4dd
DNSubramanyam <subbu.dns1998@gmail.com>
2024-09-04 21:31:04 +0530

    changes to ami block

--- aws_ami_backup/main.py
+++ aws_ami_backup/main.py
@@ -1,5 +1,5 @@
 import boto3
-import os
+from datetime import  date
 from pprint import pprint
 
 ec2 = boto3.client('ec2', region_name='us-east-1')
@@ -41,43 +41,58 @@ def tag_block(tag_key, tag_value):
 
 def initiate_ami(ins_id_list):
     print(f'initiating ami backup for the servers: {ins_id_list}')
+    for each in ins_id_list:
+        image_response = ec2.create_image(
+            InstanceId=each,
+            Name=f'{each}_{date.today().strftime("%d_%b_%Y_%H_%M")}', 
+            NoReboot=True, 
+            TagSpecifications=[
+                {'ResourceType': 'image', 
+                 'Tags': [ 
+                    { 'Key': 'string',
+                     'Value': 'string' }
+                 ]
+             }
+         ]
+      )
+        pprint(image_response['ImageId'])
+
+def main_handle():
+    print("""
+        ************************
+        * Choose your input : *
+        * a. IP address       *
+        * b. InstanceId       *
+        * c. Tag              * 
+        ************************
+        \n""")
+
+    file_name = ''
+    input_list = []
+    choice = input("Enter your choice : ")
+    if choice == 'a' or choice == 'b':
+        file_name = input("Enter the file name : ")
+        try:
+            with open(file_name, 'r') as file:
+                input_data = file.read().strip()
+            input_list = input_data.split('\n')
+            input_list = [i for i in input_list if i]
+        except Exception as error:
+            print(error)
+
+    if choice == 'a' and len(input_list) > 0:
+        ip_block(input_list)
+    elif choice == 'b' and len(input_list) > 0:
+        initiate_ami(input_list)
+    elif choice == 'c':
+        tag_key = input("Enter the tag key: ")
+        tag_value = input("Enter the tag value: ")
+        tag_block(tag_key, tag_value)
+    else:
+        print('Invalid option selected or input file is empty!!')
 
 
-print("""
-    ************************
-    * Choose your input : *
-    * a. IP address       *
-    * b. InstanceId       *
-    * c. Tag              * 
-    ************************
-    \n""")
-
-file_name = ''
-input_list = []
-choice = input("Enter your choice : ")
-if choice == 'a' or choice == 'b':
-    file_name = input("Enter the file name : ")
-    try:
-        with open(file_name, 'r') as file:
-            input_data = file.read().strip()
-        input_list = input_data.split('\n')
-        input_list = [i for i in input_list if i]
-    except Exception as error:
-        print(error)
-
-if choice == 'a' and len(input_list) > 0:
-    ip_block(input_list)
-elif choice == 'b' and len(input_list) > 0:
-    initiate_ami(input_list)
-elif choice == 'c':
-    tag_key = input("Enter the tag key: ")
-    tag_value = input("Enter the tag value: ")
-    tag_block(tag_key, tag_value)
-else:
-    print('Invalid option selected or input file is empty!!')
-
-
-
+main_handle()
 
             
         
